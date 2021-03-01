270739d094d0dbaa17c400018351b94a998c77a6
Pedro Ramos <pedrotramos97@gmail.com>
2021-03-01 10:39:26 -0300

    Readme

--- Assets/Scripts/BlocoSpawner.cs
+++ Assets/Scripts/BlocoSpawner.cs
@@ -18,13 +18,13 @@ public class BlocoSpawner : MonoBehaviour
     {
         if (gm.gameState == GameManager.GameState.GAME)
         {
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 11; i++)
             // for (int i = 0; i < 1; i++)
             {
                 for (int j = 0; j < 4; j++)
                 // for (int j = 0; j < 1; j++)
                 {
-                    Vector3 posicao = new Vector3(-7 + 2.0f * i, 4.0f - 0.8f * j);
+                    Vector3 posicao = new Vector3(-7.5f + 1.5f * i, 4.0f - 0.8f * j);
                     // Vector3 posicao = new Vector3(0, 0);
 
                     Instantiate(Bloco, posicao, Quaternion.identity, transform);
@@ -38,13 +38,6 @@ public class BlocoSpawner : MonoBehaviour
         if (transform.childCount <= 0 && gm.gameState != GameManager.GameState.MENU)
         {
             gm.ChangeState(GameManager.GameState.ENDGAME);
-        }
-        if (transform.childCount <= 0 && gm.gameState == GameManager.GameState.MENU)
-        {
-            foreach (Transform child in transform)
-            {
-                GameObject.Destroy(child.gameObject);
-            }
         }
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# BrickBreaker
+# Brick Breaker!!!
+
+Trata-se de um jogo ao estilo Breakout que foi desenvolvido na disciplina de Jogos Digitais como introdução ao Unity. 
+
+Referências:
+
+1. Background: http://clipart-library.com/clipart/yckrdpjdi.htm
+2. Sprite Bola: http://www.rafaelservantez.com/writing/tutorials/art/pixel/pixel_shading_4.png
+3. Sprite Raquete: http://pixelartmaker.com/art/72bd303189fe5db
+4. Sprite Bloco: https://chezjau.studio/wp-content/uploads/2018/07/joinedblocksDlight128x64.png
+5. Música: https://freesound.org/people/frankum/sounds/384468/
